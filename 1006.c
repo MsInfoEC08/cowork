d59322367db8a0317c5fd070b7c0f1740af62e0b
wellessw <44391025+wellessw@users.noreply.github.com>
2018-10-23 14:44:13 +0800

    Add files via upload

---
+++ 1006.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int x;
+	scanf("%d",&x);
+	if(x>=90)printf("A\n");
+	 if(80<=x&&x<90)printf("B\n");
+	 if(70<=x&&x<79)printf("C\n");
+ if(60<=x&&x<69)printf("D\n");
+	else printf("E\n");
+}
